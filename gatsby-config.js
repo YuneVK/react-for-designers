75cf9ca719b874bcf10d8e5a921461ed00a86d85
Sonia Ruiz <soniaruiz@MacBook-Pro-de-Sistemas.local>
2019-04-25 18:51:22 +0200

    Lesson 10 - WIP

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `React For Designers`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `Complete courses about the best tools and design systems. Prototype and build apps with React and Swift. 60 hours of video content and resource materials. No coding experience required.`,
+    keywords: `react course, react for designers, ios development, sketch app, swift app course, arkit 2, after effects, create sketch plugin`,
     author: `@gatsbyjs`,
   },
   plugins: [
@@ -27,6 +28,13 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: 'gatsby-source-contentful', 
+      options: {
+        spaceId: 'isxe4u3tpuex',
+        accessToken: 'e2b434addffe33a16480662ed333bfca977f671ea131308cb10443d838657f52'
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/components/layout.js
+++ src/components/layout.js
@@ -11,6 +11,7 @@ import { StaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import "./layout.css"
+import Footer from "./Footer";
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -19,6 +20,16 @@ const Layout = ({ children }) => (
         site {
           siteMetadata {
             title
+            description
+            keywords
+          }
+        }
+        allContentfulLink {
+          edges {
+            node {
+              title
+              url
+            }
           }
         }
       }
@@ -27,6 +38,12 @@ const Layout = ({ children }) => (
       <>
         <Header />
         <main>{children}</main>
+        
+        
+
+        <Footer data={data}>
+          Backgrounds made in Cinema 4D, iOS app in Swift, site in React. <a href="mailto:support@designcode.io">Email us</a> to ask anything. © 2018 - Terms of Service - Privacy Policy
+        </Footer>
       </>
     )}
   />
